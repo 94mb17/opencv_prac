c7dc38bdaf569f3c4aace4a9841068b017c1be22
94mb17 <comics.101.101@gmail.com>
2024-01-06 17:32:37 +0530

    change

--- ai/pose_det.py
+++ ai/pose_det.py
@@ -6,7 +6,7 @@ cam_obj = cv.VideoCapture(0)
 cam_obj.set(cv.CAP_PROP_FRAME_WIDTH, 640)
 cam_obj.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
 cam_obj.set(cv.CAP_PROP_FPS, 30)
-cam_obj.set(cv.CAP_PROP_FOURCC, cv.VideoWriter_fourcc(*'MJPG'))
+cam_obj.set(cv.CAP_PROP_FOURCC, cv.VideoWriter_fourcc(*'DIVXs'))
 
 pose = mp.solutions.pose.Pose(static_image_mode=False,
                                model_complexity=1,
@@ -22,16 +22,16 @@ while True:
     results = pose.process(frameRGB)
     land = []
     if results.pose_landmarks != None:
-        # mp.solutions.drawing_utils.draw_landmarks(frame, results.pose_landmarks, mp.solutions.pose.POSE_CONNECTIONS)
-        for l in results.pose_landmarks.landmark:
-            land.append((int(l.x*640), int(l.y*480)))
+        mp.solutions.drawing_utils.draw_landmarks(frame, results.pose_landmarks, mp.solutions.pose.POSE_CONNECTIONS)
+        # for l in results.pose_landmarks.landmark:
+        #     land.append((int(l.x*640), int(l.y*480)))
     
     #nose
-        cv.circle(frame, (land[0]), 5, (160, 0, 155), -1)
-    #eyes
-        cv.circle(frame, (land[2]), 5, (120, 90, 155), -1)
-        cv.circle(frame, (land[5]), 5, (120, 90, 155), -1)
-        
+    #     cv.circle(frame, (land[0]), 5, (160, 0, 155), -1)
+    # #eyes
+    #     cv.circle(frame, (land[2]), 5, (120, 90, 155), -1)
+    #     cv.circle(frame, (land[5]), 5, (120, 90, 155), -1)
+
     cv.imshow('Frame', frame)
 
     if cv.waitKey(1) & 0xFF == ord('d'):
